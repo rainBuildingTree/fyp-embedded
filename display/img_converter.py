0bfb6bacc01ea14ea70a6d0b6ce271150efe407a
rainBuildingTree <rbtree0303@gmail.com>
2024-12-30 17:31:34 +0800

    fix: converter bug

--- display/img_converter.py
+++ display/img_converter.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def convert_image_to_data(path):
     image = Image.open(path)
-    resized_image = Image.resize((128, 128))
+    resized_image = image.resize((128, 128))
     np_image = np.array(resized_image)
     r = np_image[:,:,0] >> 3
     g = np_image[:,:,1] >> 2
