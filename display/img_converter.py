f0d1689514ba5316cfc0fb5a892d33fabccff8d0
rainBuildingTree <rbtree0303@gmail.com>
2024-12-30 17:55:47 +0800

    add converter feats

--- display/img_converter.py
+++ display/img_converter.py
@@ -1,6 +1,11 @@
 from PIL import Image
 import numpy as np
 
+def split_data_to_8bit(data):
+    hi = (data >> 8) & 0xFF
+    lo = data & 0xFF
+    return [hi, lo]
+
 def convert_image_to_data(path):
     image = Image.open(path)
     resized_image = image.resize((128, 128))
@@ -9,4 +14,19 @@ def convert_image_to_data(path):
     g = np_image[:,:,1] >> 2
     b = np_image[:,:,2] >> 3
     rgb = (r << 11) | (g << 5) | b
-    return rgb.flatten().tolist()
+    flat_rgb = rgb.flatten().tolist()
+    final_data = []
+    for data in flat_rgb:
+        final_data.extend(split_data_to_8bit(data))
+    return final_data
+
+def convert_and_save(path_from, path_to):
+    list_data = convert_image_to_data(path_from)
+    with open(path_to, 'w') as file:
+        for value in list_data:
+            file.write("%d\n" % value)
+
+def load_list_data(path):
+    with open(path, 'r') as file:
+        loaded_list = [int(line.strip()) for line in file]
+        return loaded_list
