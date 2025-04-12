035b05a2bc247f02d4d7212ae9576781509f957f
rainBuildingTree <rbtree0303@gmail.com>
2025-04-12 13:49:09 +0800

    250412

--- camera/video_sender.py
+++ camera/video_sender.py
@@ -3,14 +3,14 @@ import asyncio
 import websockets
 
 async def send_video():
-    async with websockets.connect('ws://172.29.164.229:8000/video') as websocket:
+    with asyncio.wait_for(websockets.connect('ws://172.29.164.229:8000/video'), 60):
         cap = cv2.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            _, buffer = cv2.imencode('.jpg', frame)
-            await websocket.send(buffer.tobytes())
+        #while True:
+            #ret, frame = cap.read()
+            #if not ret:
+            #    break
+            #_, buffer = cv2.imencode('.jpg', frame)
+            #await websocket.send(buffer.tobytes())
         cap.release()
 
 asyncio.run(send_video())
