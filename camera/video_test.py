1c122e5ac2dd0a79f9819901816ec89ac7ae0de7
rbtree <rbtree0303@gmail.com>
2025-04-13 22:06:57 +0800

    a

--- camera/video_test.py
+++ camera/video_test.py
@@ -6,7 +6,7 @@ cam.configure(cam.create_video_configuration(main={"size": (640, 480)}))
 cam.start_and_record_video("test.mp4", duration=10)
 print('start')
 subprocess.run([
-    'ffmpeg', '-y' '-i', 'test.mp4',
+    'ffmpeg', '-y', '-i', 'test.mp4',
     '-vf', 'transpose=1',
     'test.mp4'
 ])
